86934a63daead240f87c4ce85a99ab12e045c8cf
Brody <121767457+PAN-DA-BOI@users.noreply.github.com>
2025-01-27 20:58:00 -0700

    updated

--- scripts/host script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set IP=192.168.1.5
-gzdoom.exe -host 2 -warp 1 -file wads\PUSS24.wad -extratic
-echo Hosting game at IP: %IP%
-pause
--- scripts/join script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set IP=192.168.1.5
-gzdoom.exe -join %IP% -file wads\PUSS24.wad -extratic
-
-pause
